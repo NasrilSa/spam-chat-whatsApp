43a03e4de9d7c532cf98e7055db9ebd9762d49e8
NasrilSa <157220032+NasrilSa@users.noreply.github.com>
2024-03-09 12:51:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyautogui as pg
 import time
 time.sleep(10)
 
-for i in range(1000): #humlah pesan yang ingin dikirim
+for i in range(1000): #jumlah pesan yang ingin dikirim
     pg.write("test") #pesan yang ingin ditulis
     pg.press('Enter') 
 
